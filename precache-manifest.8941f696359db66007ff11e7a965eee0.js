46f76991ba67c34cb2bda892bb74592adb8f88ff
Denis Schüle <faberds@deniss-mbp.home>
2019-12-19 06:14:26 +0100

    Updates

---
+++ precache-manifest.8941f696359db66007ff11e7a965eee0.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60ec7013a498de00b1bb36fbf344a9ac",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "d0973708ea6a9cb6f1a8",
+    "url": "/Portfolio/static/css/main.ee1f1eb4.chunk.css"
+  },
+  {
+    "revision": "67d5650c5797f8b60bfa",
+    "url": "/Portfolio/static/js/2.395014a5.chunk.js"
+  },
+  {
+    "revision": "1fdd8477b82dbd0dc30ff62b40004fea",
+    "url": "/Portfolio/static/js/2.395014a5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d0973708ea6a9cb6f1a8",
+    "url": "/Portfolio/static/js/main.39cfcff3.chunk.js"
+  },
+  {
+    "revision": "93d806d1a046e00ac01d",
+    "url": "/Portfolio/static/js/runtime-main.c7335eb8.js"
+  },
+  {
+    "revision": "7f7b8384fee870b2c720f42322a2efb9",
+    "url": "/Portfolio/static/media/SharePoint.7f7b8384.png"
+  },
+  {
+    "revision": "ba3a903611f194635c1f62b96fe3acdd",
+    "url": "/Portfolio/static/media/bootstrap.ba3a9036.png"
+  },
+  {
+    "revision": "b382e3abff76833b4c7354408cb892c7",
+    "url": "/Portfolio/static/media/campus02.b382e3ab.png"
+  },
+  {
+    "revision": "99716a71adea9e4e0d73a7f8b5750b0f",
+    "url": "/Portfolio/static/media/campus02Logo.99716a71.png"
+  },
+  {
+    "revision": "5ce87e2ba1a15f6cf4321b70fb510b46",
+    "url": "/Portfolio/static/media/code.5ce87e2b.jpeg"
+  },
+  {
+    "revision": "3d000bebfe04b1361b739f3aab10826e",
+    "url": "/Portfolio/static/media/codefactory.3d000beb.png"
+  },
+  {
+    "revision": "bf60f28a70f827ee457e0f951d645003",
+    "url": "/Portfolio/static/media/codefactoryLogo.bf60f28a.jpg"
+  },
+  {
+    "revision": "c9d519d51fadf7f2d6cf4646699e13cf",
+    "url": "/Portfolio/static/media/css.c9d519d5.png"
+  },
+  {
+    "revision": "e84c89746e2f64e39e88910c93edc5f3",
+    "url": "/Portfolio/static/media/dccs.e84c8974.png"
+  },
+  {
+    "revision": "9fd0a888b291c5360eb58bf373661d33",
+    "url": "/Portfolio/static/media/dccsLogo.9fd0a888.jpg"
+  },
+  {
+    "revision": "c8b3ff2350d283f6ecd16ec159366190",
+    "url": "/Portfolio/static/media/faberLogo.c8b3ff23.png"
+  },
+  {
+    "revision": "9aec64054733a787ab5e9b04b8366e16",
+    "url": "/Portfolio/static/media/flow.9aec6405.png"
+  },
+  {
+    "revision": "e9b80f93ce1ce6ebfbc35cc1785cb67b",
+    "url": "/Portfolio/static/media/html.e9b80f93.png"
+  },
+  {
+    "revision": "652fdb659a681116811612e0b9e25354",
+    "url": "/Portfolio/static/media/java.652fdb65.png"
+  },
+  {
+    "revision": "42426e79af439420bcb60505e0d608d3",
+    "url": "/Portfolio/static/media/jquery.42426e79.png"
+  },
+  {
+    "revision": "c2e0ad94286c2488ea3a1d76b7197669",
+    "url": "/Portfolio/static/media/js.c2e0ad94.png"
+  },
+  {
+    "revision": "cfed49694af0b37fd6876b2bc044dace",
+    "url": "/Portfolio/static/media/laptopC.cfed4969.png"
+  },
+  {
+    "revision": "3b9cacc54f649b36b9fafd630370d232",
+    "url": "/Portfolio/static/media/laravel.3b9cacc5.png"
+  },
+  {
+    "revision": "df67383d536202dbf814576b0393cd39",
+    "url": "/Portfolio/static/media/nintex.df67383d.png"
+  },
+  {
+    "revision": "4c6c0929cc58f241847a744b2de75e2f",
+    "url": "/Portfolio/static/media/node.4c6c0929.png"
+  },
+  {
+    "revision": "71451c2962309d95ac03ee9c176b82e9",
+    "url": "/Portfolio/static/media/phone.71451c29.jpeg"
+  },
+  {
+    "revision": "cfb123ceb3280fae2200bc0e711818e7",
+    "url": "/Portfolio/static/media/php.cfb123ce.png"
+  },
+  {
+    "revision": "bc3bb5d4e194f73e418dabb33e11de15",
+    "url": "/Portfolio/static/media/powerapps.bc3bb5d4.png"
+  },
+  {
+    "revision": "a4afafc3dfc5c9f1d163a2de47fad8f6",
+    "url": "/Portfolio/static/media/powershell.a4afafc3.png"
+  },
+  {
+    "revision": "a1766471e07bbdf2d665e2f49caa5ebd",
+    "url": "/Portfolio/static/media/react.a1766471.png"
+  },
+  {
+    "revision": "3d8249adb0d2ddfb8faddb06e621958b",
+    "url": "/Portfolio/static/media/sass.3d8249ad.png"
+  },
+  {
+    "revision": "6ef6da7f8ef88cadd76ef46f366dcc43",
+    "url": "/Portfolio/static/media/spfx.6ef6da7f.png"
+  },
+  {
+    "revision": "525aab015d2e35161af52674abc2034d",
+    "url": "/Portfolio/static/media/spring.525aab01.png"
+  },
+  {
+    "revision": "f27119c20951b473f2de80c000d60fa8",
+    "url": "/Portfolio/static/media/symfony.f27119c2.png"
+  }
+]);
